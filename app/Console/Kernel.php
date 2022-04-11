c9bf7fd5ca419fcb4dc768c2f7c9b643bbe0731c
Ryuukaiser <gauvu107@gmail.com>
2022-04-11 19:48:44 +0700

    bản 8

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('scrape:truyen')->weeklyOn(1,'8:00');
+
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,14 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
+use Admin\FCController;
+use Admin\DCController;
+use Admin\DrinkController;
+use Admin\FoodController;
+use Admin\StaffController;
+use Admin\CusController;
+use Admin\UserController;
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,9 +21,31 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+// insret catergory
+Route::get('/home','HomeController@getHome');
+Route::post('/home','HomeController@postLogin');
+Route::get('/menu','HomeController@getAll');
+Route::get('/logout','HomeController@getLogout');
+Route::get('/menu/{idcatergory}','HomeController@getMenu');
+Route::get('/user/{getUser}','HomeController@getUser');
+//login
+Route::post('login', 'HomeController@postLogin');
+//chi tiet san pham
+Route::get('/home/food','Food\FoodDetailController@getFood');
+Route::get('/home/food/{ID_Food}','Food\FoodDetailController@getFoodID');
+//them vao gio hang
+Route::get('/home/food/cart/{ID_Food}','Food\CartFoodController@CartFood');
+//them sua xoa
+Route::resource('admin/foodcategory', FCController::class);
+Route::resource('admin/food', FoodController::class);
+Route::resource('admin/drinkcategory', DCController::class);
+Route::resource('admin/drink', DrinkController::class);
+Route::resource('admin/staff', StaffController::class);
+Route::resource('admin/customer', CusController::class);
+Route::resource('admin/user', UserController::class);
 
 
-
+//register
+Route::post('register', 'HomeController@postRegister');
+?>
